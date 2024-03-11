b28da0d93841e10b08a551f0aded5db7cb618b01
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-03-11 19:16:56 +0530

    Add MongoDB connection and environment variables

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    ); // mongoose gives you returned object
+    console.log(
+      `MongoDb connected, DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Error occurred while connecting to database", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config({ path: "./env" }); // it does not maintain consistency for import statement
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`); // using template string
+    app.on("error", (error) => {
+      // using arrow function to catch error occured in connecting express and database
+      console.log("Error occured in server", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      // using arrow function to log the port on which server is running
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error ocuured in connectint to database", error);
+    throw err;
+  }
+})(); // using IIFE to use async/await
+*/
