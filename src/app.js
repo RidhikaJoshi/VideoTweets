c25383b33a3110ebc278ae44bbdcd9b07ebb9262
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-03-16 06:34:08 +0530

    Add user registration route and controller

--- src/app.js
+++ src/app.js
@@ -23,4 +23,17 @@ app.use(express.static("public")); // this method is used to serve static files
 
 app.use(cookieParser()); // this method is used to parse Cookie header and populate req.cookies with an object keyed by the cookie names
 
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+// since we have segregated rotes and controllers so we cannot use app.get() method here
+// instead we will use middleware to use the routes
+
+app.use("/api/v1/users", userRouter);
+// it is good practice to use versioning and decalring api if creating api  in routes
+
+// url will be https://localhost:8000/api/v1/users/register
+
 export default app;
