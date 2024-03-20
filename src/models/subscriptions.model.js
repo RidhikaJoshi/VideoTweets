88489ca7ea45d46cecd3ec394c4b9e6dffef77f9
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-03-20 06:16:16 +0530

    Add subscription model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: [
+      // one who is subscribing
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
