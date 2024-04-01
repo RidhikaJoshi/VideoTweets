c2973127a1e1e8c9a89de432aab975755d3a847b
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-04-01 17:06:24 +0530

    Update package.json and enable console log in db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     ); // mongoose gives you returned object
-    // console.log(
-    //   `MongoDb connected, DB HOST: ${connectionInstance.connection.host}`
-    // );
+    console.log(
+      `MongoDb connected, DB HOST: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("Error occurred while connecting to database", error);
     process.exit(1);
