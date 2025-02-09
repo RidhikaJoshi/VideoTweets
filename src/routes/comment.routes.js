0733e2f3403a842d11a071cdfa8a466d44391b86
RidhikaJoshi <ridhikasmrh@gmail.com>
2025-02-09 19:03:57 +0530

    Add route to retrieve a comment by ID in comment.routes.js

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -5,12 +5,13 @@ import {
   addComment,
   deleteComment,
   updateComment,
+  getCommentById
 } from "../controllers/comment.controller.js";
 
 const router = Router(); // this method is used to create a new router object
 router.use(verifyJWT);
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
-router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/c/:commentId").get(getCommentById).delete(deleteComment).patch(updateComment);
 
 export default router;
