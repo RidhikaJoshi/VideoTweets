5f35f5f936d1596b6bd16b99aabb523f2b23729b
RidhikaJoshi <ridhikasmrh@gmail.com>
2025-02-11 17:41:04 +0530

    Update environment configuration and Docker setup for Redis integration

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,10 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     ); // mongoose gives you returned object
-    console.log(
-      `MongoDb connected, DB HOST: ${connectionInstance.connection.host}`
-    );
+    // console.log(
+    //   `MongoDb connected, DB HOST: ${connectionInstance.connection.host}`
+    // );
+    console.log("MongoDb connected");
   } catch (error) {
     console.log("Error occurred while connecting to database", error);
     process.exit(1);
